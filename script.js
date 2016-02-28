1315306b5ab0c5eb64aa146f0732709b7edb6f7c
marcus gray <abundanceisfree@gmail.com>
2016-02-28 11:14:41 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+function startGame() {
+
+  for (var i = 1; i <= 9; i = i +1) {
+      clearBox(i);
+  }
+  document.turn = "Tic";
+  document.winner = null;
+  setMessage(document.turn + " your Up Yo!");
+}
+
+function setMessage(msg) {
+  document.getElementById("message").innerText = msg;
+}
+
+function nextMove(square) {
+
+    if (document.winner != null)  {
+        setMessage(document.winner + " COLD Krushed it Yo!");
+    } else if (square.innerText == "") {
+    square.innerText = document.turn;
+    switchTurn();
+    } else {
+    setMessage("No Can Do Yo!");
+  }
+}
+
+function switchTurn() {
+
+    if (checkForWinner(document.turn)) {
+        setMessage("Congratulations, " + document.turn + "! You Win Yo!");
+        document.winner = document.turn;
+    } else if (document.turn == "Tic") {
+        document.turn = "Tac";
+        setMessage("It's " + document.turn + " 's turn Yo!" );
+
+    } else {
+      document.turn = "Tic";
+      setMessage("It's " + document.turn + " 's turn Yo!" );
+    }
+
+
+  }
+
+
+function checkRow(a, b, c, move) {
+    var result = false;
+    if (getBox(a) == move && getBox(b) == move && getBox(c) == move){
+        result = true;
+   }
+    return result;
+  }
+function getBox(number) {
+  return document.getElementById("s" + number).innerText;
+}
+
+function clearBox(number) {
+    document.getElementById("s" + number).innerText = "";
+}
+
+function checkForWinner(move) {
+    var result = false;
+    if (checkRow(1, 2, 3, move) ||
+        checkRow(4, 5, 6, move) ||
+        checkRow(7, 8, 9, move) ||
+        checkRow(1, 4, 7, move) ||
+        checkRow(2, 5, 8, move) ||
+        checkRow(3, 6, 9, move) ||
+        checkRow(1, 5, 9, move) ||
+        checkRow(3, 5, 7, move)) {
+
+        result = true;
+    }
+    return result;
+}
+
+
